b677b92aec4ec73c1ead1b01beeda89898f999c8
Stanley Essuman Arthur <stanleyokyemanarthur@gmail.com>
2024-03-11 15:08:35 +0000

    initial commit

--- components/ReviewsData.js
+++ components/ReviewsData.js
@@ -3,35 +3,35 @@ export const reviews = [
       id: 1,
       image:
       '/assets/images/photo4.jpg',
-      name: 'Daniel Lee',
+      name: 'Leo Rodriguez',
       text: "I appreciate the commitment of Glorified Touch Care to provide reliable and around-the-clock care for seniors. Their team has become an integral part of my mother's support system.",
     },
     {
       id: 2,
       image:
       '/assets/images/photo3.jpg',
-      name: 'Karen Wilson',
+      name: 'Mia Sullivan',
       text: "The respite home care services provided by Glorified Touch Care allowed me to take a much-needed break while ensuring my husband received top-notch care. I am grateful for their dedication.",
     },
     {
       id: 3,
       image:
       '/assets/images/photo7.jpg',
-      name: 'Michael Rodriguez',
+      name: 'Oliver Nguyen',
       text: "Glorified Touch Care understands the unique needs of seniors, especially those facing health challenges. Their in-home dementia care has made a significant difference in my mother's daily life.",
     },
     {
       id: 4,
       image:
       '/assets/images/photo8.jpg',
-      name: 'Jennifer Adams',
+      name: 'Jennifer Patel',
       text: "Reliable hospital care is crucial for seniors, and Glorified Touch Care delivers exactly that. Their caregivers are not just professionals; they're companions who go above and beyond for their clients.",
     },
     {
       id: 5,
       image:
         '/assets/images/photo9.jpg',
-      name: 'Robert Martinez',
+      name: 'Montgomery Martinez',
       text: "I couldn't be happier with the care my grandfather receives from Glorified Touch Care. The staff is not only professional but also treats him with the utmost respect and kindness",
     },
   ];
