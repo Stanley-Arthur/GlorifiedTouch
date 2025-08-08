f7fffa6317f9bbca30db447b303903a02f1e2f5a
Stanley Essuman Arthur <stanleyokyemanarthur@gmail.com>
2025-08-08 17:39:31 +0000

    first commit

--- pages/testimonials.jsx
+++ pages/testimonials.jsx
@@ -1,4 +1,4 @@
-
+"use client";
 
 import { reviews } from '@/components/ReviewsData';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
@@ -32,19 +32,19 @@ const Testimonials = () => {
           {reviews.map((review) => (
             <SplideSlide key={review.id} className="flex items-center">
               <div className="flex justify-center items-center w-1/2  rounded-full overflow-hidden transition-opacity duration-100 ease-in-out">
-			   <Image  src={review.image} width={200} height={200} alt="" className="rounded-full h-16 w-16 object-cover" />
-			  </div>
+                <Image  src={review.image} width={200} height={200} alt="" className="rounded-full h-16 w-16 object-cover" />
+              </div>
               <div >
                 <div className="w-1/2 ml-8">
-					<p className=" text-2xl font-semibold mb-2">{review.name}</p>
-					<p className=" text-gray-600">{review.text}</p>
+                  <p className=" text-2xl font-semibold mb-2">{review.name}</p>
+                  <p className=" text-gray-600">{review.text}</p>
                   <div className="rating">
                     <span className="star">&#9733;</span>
                     <span className="star">&#9733;</span>
                     <span className="star">&#9733;</span>
                     <span className="star">&#9733;</span>
                     <span className="star">&#9734;</span>
-				   </div>
+                  </div>
                 </div>
               </div>
             </SplideSlide>
