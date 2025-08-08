cba5f242e1877125b1f18a46d1580f76c7e34ba5
Stanley Essuman Arthur <stanleyokyemanarthur@gmail.com>
2025-08-08 17:58:40 +0000

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,44 @@
-
+import dynamic from 'next/dynamic';  // <-- add this import
 import Advantages from "../components/Advantages";
 import Articles from "../components/Articles";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Services from "../components/Services";
-import Testimonials from "./testimonials";
+// import Testimonials from "./testimonials";  <-- remove this import
 import data from "../constants/data";
 import Head from "next/head";
 import DriftWidget from "@/components/DriftWidget";
+
+// Dynamically import Testimonials with SSR disabled (client-side only)
+const Testimonials = dynamic(() => import('./testimonials'), { ssr: false });
 
 export default function Home(props) {
 	return (
 		<div>
 			<Head>
-
 				<title>GLORIFIED TOUCH CARE - Your Trusted Health Partner</title>
-
-				<meta
-					name="viewport"
-					content="initial-scale=1.0, width=device-width"
-				/>
-				<link rel="shortcut icon" href="/public/favicon.ico" />
-
-
-
-
-
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<link rel="shortcut icon" href="/favicon.ico" />
 			</Head>
+
 			{/* <Navbar /> */}
 
 			<div>
-
-				{/* <Drift
-				appId="hhgg7b7c3tag"
-				userId="USER_ID_IF_AVAILABLE"
-				attributes={{ email: "glorifiedtouchcare@gmail.com", company: "Glorified Touch Care" }}
-
-				eventHandlers={[  ]}
-				style={{
-					backgroundColor: "#0000ff",
-					color: "#333333", 
-					borderRadius: "8px", 
-					
-				  }}
-			/> */}
-
 				<DriftWidget
 					appId="tmumngnvac5d"
 					userId="USER_ID_IF_AVAILABLE" // optional
 					attributes={{
 						email: "stanleyokyemanarthur@gmail.com",
-						company: "Glorified Touch Care"
+						company: "Glorified Touch Care",
 					}}
-					eventHandlers={[
-						/* { event: 'ready', handler: () => {} } */
-					]}
+					eventHandlers={[]}
 					style={{
-						backgroundColor: "#0a0a23", // blue-black shade
-						color: "#ffffff", // make text visible on dark background
-						borderRadius: "8px"
+						backgroundColor: "#0a0a23",
+						color: "#ffffff",
+						borderRadius: "8px",
 					}}
 				/>
-
 			</div>
 
 			{/* Hero Section */}
@@ -71,19 +47,20 @@ export default function Home(props) {
 			<Services services={data.services} />
 			{/* Advantages */}
 			<Advantages />
-			{/* To do - Carousel */}
+			
+			{/* Testimonials Section - now client-side only */}
 			<div id="testimonials">
 				<Testimonials />
 			</div>
+
 			{/* Articles */}
 			<Articles articles={data.articles} />
 			{/* Footer */}
 			{/* <Footer /> */}
 		</div>
 	);
-
-
 }
+
 export async function getStaticProps(context) {
 	return {
 		props: {}, // will be passed to the page component as props
