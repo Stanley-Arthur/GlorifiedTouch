10d346f56c6db92dd40438dff1a30fe1f14e7623
Stanley Essuman Arthur <stanleyokyemanarthur@gmail.com>
2025-08-08 16:02:20 +0000

    initial commit

--- components/HamMenu.jsx
+++ components/HamMenu.jsx
@@ -1,10 +1,10 @@
-'use client';
+// 'use client';
 
-import React from "react";
-import { HamburgerButton } from "react-hamburger-button";
-import * as styles from "../styles/HamMenu.module.css";
-const HamMenu = ({ clickHandler }) => {
-	return <div></div>;
-};
+// import React from "react";
+// import { HamburgerButton } from "react-hamburger-button";
+// import * as styles from "../styles/HamMenu.module.css";
+// const HamMenu = ({ clickHandler }) => {
+// 	return <div></div>;
+// };
 
-export default HamMenu;
+// export default HamMenu;
